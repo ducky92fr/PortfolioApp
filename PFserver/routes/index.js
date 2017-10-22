637ea4345e7a8ff7fbee98219f3463e4b54cafe1
Jad ch <jad.chalita@gmail.com>
2017-10-22 12:13:06 +0200

    Added two basic API functions

--- PFserver/routes/index.js
+++ PFserver/routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* Testing */
-router.get('/', function(req, res, next) {
-  res.json({ test: 'Hello' });
-});
+router.get('/', function (req, res, next) {
+  getLastPrices(['snap', 'fb']).then((data) => {
+    res.json(data)
+  })
+})
 
 module.exports = router;
+
+
+
+// IEX API
+const axios = require('axios')
+
+const IEX = axios.create({
+  baseURL: 'https://api.iextrading.com/1.0'
+})
+
+
+// Get real-time price
+// Last provides trade data for executions on IEX. It is a near real time,intraday API that provides IEX last sale price, size and time.
+function getLastPrice(ticker) {
+  let url = `/tops/last?symbols=${ticker}`
+  return IEX.get(url).then(response => response.data)
+}
+
+function getLastPrices(tickers) {
+  let url = `/tops/last/?symbols=${tickers[0]}`
+  let length= tickers.length
+  for (let i = 1; i < length; i++) {
+    url += ','+ tickers[i]
+  }
+  return IEX.get(url).then(response => response.data)
+}
