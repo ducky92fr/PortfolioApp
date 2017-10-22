5e33038f4af3907955112784f6aa51b5a992429a
Jad ch <jad.chalita@gmail.com>
2017-10-22 19:22:45 +0200

    Created Portfolio db model

---
+++ PFserver/models/Portfolio.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+//  Portfolio model Schema. The cash portion of the portfolio
+// will be saved in startingPortfolio, currentPortfolio, and transactions
+// as a stock, ie: {stockTicker: pfAPPCASH (not a real ticker and unlikely to become one),
+// number, and an unchanging price of 1}. 
+
+
+const portfolioSchema = new Schema({
+  userID: String,
+  startingPortfolio: [{
+    stockTicker: String,
+    number: Number,
+    buyPrice: Number
+  }],
+  currentPortfolio: [{
+    stockTicker: String,
+    number: Number,
+    buyPrice: Number,
+    breakevenPrice: Number
+  }],
+  transactions: [{
+    date: Date,
+    prePortfolio: [{
+      stockTicker: String,
+      number: Number
+    }],
+    postPortfolio: [{
+      stockTicker: String,
+      number: Number
+    }],
+    change: [{
+      stockTicker: String,
+      buy: Boolean, // true if the user buys more, false if the user sells
+      buyPrice: Number,
+      transactionPrice: Number
+    }]
+  }]
+})
+
+const Portfolio = mongoose.model('Portfolio', portfolioSchema)
+
+module.exports = Portfolio
