d2ba2697e47d256fd3a3962386f1a5a0e226e6ae
Jad ch <jad.chalita@gmail.com>
2017-10-22 12:11:52 +0200

    Bug fix: was trying to render views with no view engine

--- PFserver/app.js
+++ PFserver/app.js
@@ -9,13 +9,11 @@ var index = require('./routes/index');
 
 var app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 
@@ -27,14 +25,6 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// Todo: Add error handler
 
 module.exports = app;
