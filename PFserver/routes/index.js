7b62de7d2549dab651cc4efbd20b3b1ba67fa770
Jad ch <jad.chalita@gmail.com>
2017-10-22 17:42:52 +0200

    Added standard style

--- PFserver/routes/index.js
+++ PFserver/routes/index.js
@@ -3,14 +3,12 @@ var router = express.Router()
 
 /* Testing */
 router.get('/', function (req, res, next) {
-  getLastPrices(['snap', 'fb']).then((data) => {
+  getLastPrices(['snap', 'fb', 'aapl']).then((data) => {
     res.json(data)
   })
 })
 
-module.exports = router;
-
-
+module.exports = router
 
 // IEX API
 const axios = require('axios')
@@ -19,19 +17,22 @@ const IEX = axios.create({
   baseURL: 'https://api.iextrading.com/1.0'
 })
 
-
 // Get real-time price
 // Last provides trade data for executions on IEX. It is a near real time,intraday API that provides IEX last sale price, size and time.
-function getLastPrice(ticker) {
+function getLastPrice (ticker) {
   let url = `/tops/last?symbols=${ticker}`
   return IEX.get(url).then(response => response.data)
 }
 
-function getLastPrices(tickers) {
+function getLastPrices (tickers) {
   let url = `/tops/last/?symbols=${tickers[0]}`
-  let length= tickers.length
+  let length = tickers.length
   for (let i = 1; i < length; i++) {
-    url += ','+ tickers[i]
+    url += ',' + tickers[i]
   }
   return IEX.get(url).then(response => response.data)
 }
+
+// Todo:
+//   - format time in json responses
+//   - find other data to query
