fc26675b6b99f0891d39d7b1d739eacd117396db
Jad ch <jad.chalita@gmail.com>
2017-10-24 16:37:50 +0200

    Coded addPortfolioPage component

--- portfix/src/components/AddPortfolioPage.vue
+++ portfix/src/components/AddPortfolioPage.vue
@@ -2,43 +2,75 @@
   <div>
     <navbar></navbar>
     <div class="addPortfolioPage">
-      <h1>{{ msg }}</h1>
+      <h1>Create a new Portfolio</h1>
+      <template>
+        <v-form class='form' v-model="valid" ref="form" lazy-validation>
+          <v-text-field
+            label="Portfolio Name"
+            v-model="name"
+            :rules="nameRules"
+            required
+          ></v-text-field>
+          <v-btn
+            @click="submit"
+            :disabled="!valid"
+          >
+            Create Portfolio
+          </v-btn>
+        </v-form>
+      </template>
     </div>
   </div>
 </template>
 
 <script>
+import { checkUser } from '@/api'
 import NavBar from './tinyComponents/navbar'
 export default {
   name: 'AddPortfolioPage',
   data () {
     return {
-      msg: ''
+      msg: '',
+      name: '',
+      valid: false,
+      user: this.$root.user,
+      nameRules: [
+        (v) => !!v || 'Portfolio name is required',
+      ]
+    }
+  },
+  methods: {
+    submit () {
+
     }
   },
   components: {
     navbar: NavBar
+  },
+  created () {
+    checkUser(this.$root)
+    this.user = this.$root.user
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .addPortfolioPage {
+    margin: 5vh 10vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  form {
+    margin-top: 5vh;
+    width: 30%;
+  }
   h1 {
     font-weight: normal;
+    font-size: 3em;
   }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
+  button {
+    margin-top: 3vh;
   }
 </style>
