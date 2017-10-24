5d67b4012ddf621297b2cdac4af4e62fff000119
Jad ch <jad.chalita@gmail.com>
2017-10-24 11:01:47 +0200

    Created Sign-up component with vuetify

--- portfix/src/components/SignUp.vue
+++ portfix/src/components/SignUp.vue
@@ -1,16 +1,39 @@
 <template>
-  <div class="hello">
-  <pre>{{$data}}</pre>
+  <div class="signupPage">
     <p v-if='error'>
       {{error.message}}
     </p>
     <h1>Sign-up</h1>
-    <form @submit.prevent="signup">
-      <input v-model="name" required placeholder="name">
-      <input v-model="username" required placeholder="username">
-      <input v-model="password" required placeholder="password">
-      <button>Create New Account</button>
-    </form>
+    <template>
+      <v-form class='signupForm' v-model="valid" ref="form" lazy-validation>
+        <v-text-field
+          label="Name"
+          v-model="name"
+          :rules="nameRules"
+          required
+        ></v-text-field>
+        <v-text-field
+          label="Username"
+          v-model="username"
+          :rules="usernameRules"
+          required
+        ></v-text-field>
+        <v-text-field
+          label="Password"
+          v-model="password"
+          :rules="passwordRules"
+          required
+        ></v-text-field>
+
+        <v-btn
+          @click="signup"
+          :disabled="!valid"
+        >
+          Create New Account
+        </v-btn>
+        <v-btn @click="clear">clear</v-btn>
+      </v-form>
+    </template>
   </div>
 </template>
 
@@ -23,7 +46,19 @@ export default {
       name: '',
       username: '',
       password: '',
-      error: null
+      error: null,
+      valid: false,
+      nameRules: [
+        (v) => !!v || 'Name is required',
+      ],
+      usernameRules: [
+        (v) => !!v || 'Username is required',
+        (v) => v && v.length > 4 || 'Username must be longer than 4 characters'
+      ],
+      passwordRules: [
+        (v) => !!v || 'Password is required',
+        (v) => v && v.length > 6 || 'Password must be longer than 6 characters'
+      ]
     }
   },
   methods: {
@@ -39,27 +74,44 @@ export default {
         this.error = err
         console.error('Signup error ', err)
       })
+    },
+    clear () {
+      this.$refs.form.reset()
     }
   }
 }
 </script>
 
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  .signupPage {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  .signupForm {
+    width: 30%;
+    margin-left:  
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  h1, h2 {
+    font-weight: normal;
+    font-size: 4em;
+    margin: 5vh auto 5vh 10vw;
+  }
 
-a {
-  color: #42b983;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
 </style>
