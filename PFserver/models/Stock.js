aea5895f362e99272bda4adead8190a6dc1a542b
Jad ch <jad.chalita@gmail.com>
2017-10-28 15:59:41 +0200

    Created the Stock model

--- PFserver/models/Stock.js
+++ PFserver/models/Stock.js
@@ -5,7 +5,51 @@ const Schema = mongoose.Schema
 // More fundamental data and descriptions to be included though, for a potential stock screener.
 
 const stockSchema = new Schema({
-  ticker: String
+  ticker: String,
+  companyName: String,
+  sector: String,
+  price: {
+    latestPrice: String,
+    latestUpdate: String
+  },
+  company: {
+    CEO: String,
+    description: String
+  },
+  stats: {
+    marketcap: String,
+    beta: String,
+    week52high: String,
+    week52low: String,
+    week52change: String,
+    dividendRate: String,
+    dividendYield: String,
+    latestEPS: String,
+    latestEPSDate: String,
+    returnOnEquity: String,
+    EBITDA: String,
+    revenue: String,
+    grossProfit: String,
+    cash: String,
+    debt: String,
+    revenuePerShare: String,
+    returnOnAssets: String,
+    profitMargin: String,
+    priceToSales: String,
+    priceToBook: String,
+    day200MovingAvg: String,
+    day50MovingAvg: String,
+    institutionPercent: String,
+    year5ChangePercent: String,
+    year2ChangePercent: String,
+    year1ChangePercent: String,
+    ytdChangePercent: String,
+    month6ChangePercent: String,
+    month3ChangePercent: String,
+    month1ChangePercent: String,
+    day5ChangePercent: String
+  },
+  peers: [String]
 })
 
 const Stock = mongoose.model('Stock', stockSchema)
