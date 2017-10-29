c06ae775559057f9573ca8f01af7838a26874040
Jad ch <jad.chalita@gmail.com>
2017-10-29 17:02:31 +0100

    CompanyInfo first iteration

--- portfix/src/components/StockPage.vue
+++ portfix/src/components/StockPage.vue
@@ -7,13 +7,6 @@
         <h2>({{stockInfo.ticker}})</h2>
       </article>
       <companyinfo v-if="stockInfo" :company="stockInfo.company"></companyinfo>
-      <article class='company-info'>
-        <h1>Company</h1>
-        <div class='row'>
-          <h1>CEO</h1><h1>{{stockInfo.company.CEO}}</h1>
-        </div>
-        <p class='description-text'>{{stockInfo.company.description}}</p>
-      </article>
     </div>
   </div>
 </template>
@@ -60,17 +53,6 @@ export default {
   justify-content: center;
   margin-top: 5vh;
 }
-.row {
-  display: flex;
-  flex-direction: row;
-  margin: 2vh 0vw;
-}
-.row h1 {
-  font-size: 2em;
-}
-.row h1:last-child {
-  margin-left: 2vw;
-}
 h1 {
   font-size: 3em;
   margin: 0px;
@@ -80,15 +62,5 @@ h2 {
   font-size: 1.7em;
   margin: auto 0px 2px 12px;
   color: #797575;
-}
-.company-info {
-  width: 80vw;
-  margin-top: 5vh;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-}
-.description-text {
-  text-align: start;
 }
 </style>
--- portfix/src/components/tinyComponents/StockRelated/CompanyInfo.vue
+++ portfix/src/components/tinyComponents/StockRelated/CompanyInfo.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class='companyInfo'>
-    {{company.CEO}}
+  <div class='company-info'>
+    <h1>Company</h1>
+    <div class='row'>
+      <h1>CEO</h1><h1>{{company.CEO}}</h1>
+    </div>
+    <p class='description-text'>{{company.description}}</p>
   </div>
 </template>
 
@@ -12,7 +16,35 @@ export default {
 </script>
 
 <style scoped>
-.companyInfo {
-
+.company-info {
+  width: 80vw;
+  margin-top: 5vh;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.description-text {
+  text-align: start;
+}
+.row {
+  display: flex;
+  flex-direction: row;
+  margin: 2vh 0vw;
+}
+.row h1 {
+  font-size: 2em;
+}
+.row h1:last-child {
+  margin-left: 2vw;
+}
+h1 {
+  font-size: 3em;
+  margin: 0px;
+}
+h2 {
+  font-size: 2em;
+  font-size: 1.7em;
+  margin: auto 0px 2px 12px;
+  color: #797575;
 }
 </style>
