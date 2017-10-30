d49be7e904db77c1196f51de92b87ae7cd382467
Jad ch <jad.chalita@gmail.com>
2017-10-30 11:06:59 +0100

    deploy

---
+++ PFserver/bin/populateDB.js
@@ -0,0 +1,8 @@
+#! /app/bin/node
+function sayHello () {
+  console.log('*************************************')
+  console.log('*************** HELLO ***************')
+  console.log('*************************************')
+}
+sayHello()
+process.exit()
